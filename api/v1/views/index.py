f2ebe8ac70bf6837bdd2fae80ecfb33fefd91ff3
josh.94 <jeshua.cabanillas@gmail.com>
2022-05-23 20:36:53 -0500

    Add: task 4 files

---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status')
+def status_views():
+    '''Method return a json format status'''
+    data = {"status": "OK"}
+    return (jsonify(data))
