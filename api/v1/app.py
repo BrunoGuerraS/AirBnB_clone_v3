2426d5ea7d069f1ed6b95c26ba7aab62eda8ef18
josh.94 <jeshua.cabanillas@gmail.com>
2022-05-23 22:47:01 -0500

    Add: task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 ''' Start an  API'''
-from flask import Flask
+from flask import Flask, jsonify
 from os import getenv
 from models import storage
 from api.v1.views import app_views
@@ -15,7 +15,14 @@ def teardonw(self):
     return storage.close()
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    '''Handler for 404 errors'''
+    error = {"error": "Not found"}
+    return jsonify(error)
+
+
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST") if getenv("HBNB_API_HOST") else "0.0.0.0"
     port = getenv("HBNB_API_PORT") if getenv("HBNB_API_PORT") else 5000
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True, debug=True)
