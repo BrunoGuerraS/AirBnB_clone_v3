0909be3533aafc26835519eb42dcbf86fd44831b
josh.94 <jeshua.cabanillas@gmail.com>
2022-05-24 23:06:47 -0500

    Add: task 7

---
+++ api/v1/views/states.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+'''Script view for State objects that handles all default RESTFul API
+actions '''
+from flask import jsonify, abort, request
+from models import storage
+from models.state import State
+from api.v1.views import app_views
+
+
+@app_views.route('/states', methods=["GET"])
+def state_views():
+    '''Retrieves all default actions in JSON format'''
+    data = []
+    for i in storage.all().values():
+        data.append(i.to_dict())
+    return (jsonify(data))
+
+
+@app_views.route('states/<state_id>', methods=["GET"])
+def state_list(state_id):
+    '''Retrieves ID '''
+    data = storage.get(State, state_id)
+    if data is None:
+        abort(404)
+    return (jsonify(data.to_dict()))
+
+
+@app_views.route('/states/<state_id>', methods=["DELETE"])
+def delete_state(state_id):
+    '''Deletes '''
+    data = storage.get(State, state_id)
+    if data is None:
+        abort(404)
+    data.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/states', methods=["POST"])
+def post_state():
+    '''Transform HTTP body to dictionary'''
+    data = request.get_json()
+    if type(data) != dict:
+        abort(400, {'message': 'Not a JSON'})
+    if 'name' not in data:
+        abort(400, {"message": "Missing name"})
+    new_state = State(**data)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
+
+
+@app_views.route('/states/path:<state_id>', methods=["PUT"])
+def put_state(state_id):
+    '''Updates a State object'''
+    data_state = storage.get(State, state_id)
+    if data_state is None:
+        abort(404)
+    data = request.get_json()
+    if type(data) is not dict:
+        abort(400, {'message': 'Not a JSON'})
+    for key, value in data.items:
+        if key not in ["id", "creates_at", "update_at"]:
+            setattr(data_state, key, value)
+    storage.save()
+    return jsonify(data_state.to_dict()), 200
